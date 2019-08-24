0f3bae3e776aa78c769e1e18bd1a8341c29676c0
david-mateogit <davidmateo@outlook.com>
2019-08-24 10:55:39 -0400

    prepared for deployment

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const fs = require('fs');
+//const https = require('https');
+
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -9,6 +12,9 @@ const csrf = require('csurf');
 const flash = require('connect-flash');
 const multer = require('multer');
 const customStorage = require('./util/customStorage');
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -24,6 +30,9 @@ const store = new MongoDbStore({
   collection: 'sessions'
 });
 const csrfProtection = csrf();
+
+// const privateKey = fs.readFileSync('server.key');
+// const certificate = fs.readFileSync('server.cert');
 
 const upload = multer({
   storage: new customStorage({
@@ -61,6 +70,15 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
+
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, 'access.log'),
+  { flags: 'a' }
+);
+
+app.use(helmet());
+app.use(compression());
+app.use(morgan('combined', { stream: accessLogStream }));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -140,5 +158,8 @@ mongoose
   .then(result => {
     console.log('Server Connected!');
     app.listen(port);
+    // https
+    //   .createServer({ key: privateKey, cert: certificate }, app)
+    //   .listen(port);
   })
   .catch(err => console.log(err));
